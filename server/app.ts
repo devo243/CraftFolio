60d36abd8ff7c367df96f20ff6b33cdae760c4f4
Jacky <jackyc1704@gmail.com>
2024-11-24 15:28:09 -0500

    app.ts resolved

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,7 @@
 import AuthenticatingConcept from "./concepts/authenticating";
+import CommentingConcept from "./concepts/commenting";
 import FriendingConcept from "./concepts/friending";
+import InventoryConcept from "./concepts/inventorying";
 import PostingConcept from "./concepts/posting";
 import ProjectManagingConcept from "./concepts/projectmanaging";
 import SessioningConcept from "./concepts/sessioning";
@@ -10,4 +12,6 @@ export const Sessioning = new SessioningConcept();
 export const Authing = new AuthenticatingConcept("users");
 export const Posting = new PostingConcept("posts");
 export const Friending = new FriendingConcept("friends");
+export const Inventorying = new InventoryConcept("fibers");
+export const CommentOnPost = new CommentingConcept("comments_on_posts");
 export const ProjectManaging = new ProjectManagingConcept("projects");
