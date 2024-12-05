6a97e678b58ccf327f5808eb783fa4e36d528077
devo243 <cbrian@mit.edu>
2024-12-05 17:42:22 -0500

    added limit to min score

--- client/components/Post/PostFilterComponent.vue
+++ client/components/Post/PostFilterComponent.vue
@@ -22,7 +22,7 @@ const clearFilters = () => {
       <option value="beginner">Beginner Friendly</option>
     </select>
     <label for="minScore">Set minimum score:</label>
-    <input type="number" id="minScore" step="any" v-model="minScore" placeholder="0" required />
+    <input type="number" id="minScore" step="any" max="5" min="0"  v-model="minScore" placeholder="0" required />
     <button class="submit" type="submit">Apply Filters</button>
     <button class="clear" type="button" @click="clearFilters">Clear Filters</button>
   </form>
